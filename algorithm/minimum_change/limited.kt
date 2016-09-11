19018eda89b3dfe5268f68da5013703c55e897f0
Kumassy <kumassyii@gmail.com>
2016-09-11 13:53:17 +0900

    code clean up

--- algorithm/minimum_change/limited.kt
+++ algorithm/minimum_change/limited.kt
@@ -22,36 +22,16 @@ fun main(args: Array<String>){
 fun exchange_currencies(available_currencies: List<Int>, normalized_currencies_list: List<Int>): List<Int>{
   // normalized_currencies_list で与えられる正規化されたお金リストを、available_currencies 制約内で最前になるまで両替する
 
-  // m_: Map version
-  /*val m_normalized_currencies = currencies.zip(normalized_currencies_list).toMap()*/
   val m_normalized_currencies = currencies_list2Map(normalized_currencies_list)
-  /*var remaining = available_currencies.toMutableList()*/
   var remaining = currencies_list2MuttableMap(available_currencies)
 
   m_normalized_currencies.forEach{ entry ->
-    if(entry.key > 0){
-      // 残りのお金の 1 円側から it.key * it.value の額を構成できるか調べる
-      /*val pair_to_consume = currencies.zip(remaining).findLast{ it.first * it.second >= entry.key * entry.value }*/
-      val pair_to_consume = remaining.toList().findLast{ it.first * it.second >= entry.key * entry.value }
+    // 残りのお金の 1 円側から it.key * it.value の額を構成できるか調べる
+    val pair_to_consume = remaining.toList().findLast{ it.first * it.second >= entry.key * entry.value }
 
-      /*val index = when(pair_to_consume!!.first){
-        10000 -> 0
-        5000 -> 1
-        1000 -> 2
-        500 -> 3
-        100 -> 4
-        50 -> 5
-        10 -> 6
-        5 -> 7
-        1 -> 8
-        else -> -1
-      }*/
-      /*remaining[index] -= (entry.key * entry.value) / pair_to_consume!!.first*/
-      /*assert(remaining[index] >= 0)*/
-      val kind = pair_to_consume?.first ?: -1
-      remaining[kind] = remaining.getOrElse(kind){ 0 } - (entry.key * entry.value) / kind
-      assert(remaining.getOrElse(kind){ -1 } >= 0)
-    }
+    val kind = pair_to_consume!!.first
+    remaining[kind] = remaining.get(kind)!! - (entry.key * entry.value) / kind
+    assert(remaining.get(kind)!! >= 0)
   }
 
   return available_currencies.zip(remaining.values.toList()).map{
@@ -63,10 +43,6 @@ fun currencies_list2Map(list: List<Int>): Map<Int, Int>{
   return currencies.zip(list).toMap()
 }
 // http://stackoverflow.com/questions/38594304/copy-immutable-map-to-mutable-map-in-kotlin
-/*fun <K, V> Map<K, V>.toMutableCopy() = HashMap(this)*/
-/*fun <K, V> Map<K, V>.toMutableMap(): MutableMap<K, V> {
-  return HashMap(this)
-}*/
 fun currencies_list2MuttableMap(list: List<Int>): MutableMap<Int, Int>{
   val readOnlyMap = currencies_list2Map(list)
   val m: MutableMap<Int, Int> = if(readOnlyMap is MutableMap<*,*>){
@@ -76,14 +52,6 @@ fun currencies_list2MuttableMap(list: List<Int>): MutableMap<Int, Int>{
   }
   return m
 }
-  /*val m: MutableMap<Int, Int> = mutableMapOf()
-  return currencies.zip(list).fold(m) { accum, l ->
-    accum.put(l.first, l.second)
-  }
-}*/
-
-// TODO: mapOf( 12 to 12222, 32 to 123123123).toList().find{ it.first > 0 }
-// TODO: mapOf( 12 to 12222, 32 to 123123123).toList().findLast{ it.first > 0 }
 
 fun calculate_pay_amount(price: Int, available_currencies: List<Int>): List<Int>{
   var flag = false // true when currently focusing money shouldn't be used.
