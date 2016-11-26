24f99a11dda7338ddc3bf1885fdb2025cf6cc9e3
Kumassy <kumassyii@gmail.com>
2016-11-26 14:30:01 +0900

    self-yield

--- ppap/ppap.rb
+++ ppap/ppap.rb
@@ -28,6 +28,10 @@ class PPAP
   def initialize
     @ppap_space = []
     @ppaps = {}
+
+    if block_given?
+      yield self
+    end
   end
 
   def I_have_a ingredient
@@ -39,29 +43,30 @@ class PPAP
     if @ppap_space.reverse.join(' ') != synthesis
       raise 'Invalid PPAP'
     end
-
     puts "uh! #{synthesis}"
 
     method_name = @ppap_space.reverse.join('_').downcase
-    @ppaps[method_name] = create @ppap_space.dup
 
     define_singleton_method method_name do
-      @ppaps[method_name]
-      self.ppap_space = @ppaps[method_name]
+      puts "#{synthesis}"
+      if self.ppap_space.empty?
+        self.ppap_space = @ppaps[method_name].ppap_space
+      else
+        self.ppap_space.concat @ppaps[method_name].ppap_space.reverse
+      end
     end
 
+    @ppaps[method_name] = create @ppap_space.dup
     @ppap_space = []
-    # p @ppaps
-    # p self.methods false
   end
 
-  def to_s
-    if @ppap_space.length > 0
-      @ppap_space.reverse.join(' ')
-    elsif @ppaps.length > 0
-      @ppaps.join(' ')
-    end
-  end
+  # def to_s
+  #   if @ppap_space.length > 0
+  #     @ppap_space.reverse.join(' ')
+  #   elsif @ppaps.length > 0
+  #     @ppaps.join(' ')
+  #   end
+  # end
 
   private
   def create list
@@ -70,3 +75,47 @@ class PPAP
     t
   end
 end
+
+
+ppap = PPAP.new
+ppap.I_have_a "Pen"
+ppap.I_have_a "Apple"
+ppap.uh! "Apple Pen"
+ppap.I_have_a "Pen"
+ppap.I_have_a "Pineapple"
+ppap.uh! "Pineapple Pen"
+
+ppap.apple_pen
+ppap.pineapple_pen
+ppap.uh! "Pen Pineapple Apple Pen"
+
+ppap.pen_pineapple_apple_pen
+ppap.pen_pineapple_apple_pen
+ppap.uh! "Pen Apple Pineapple Pen Pen Pineapple Apple Pen"
+
+
+PPAP.new { |p|
+  p.I_have_a "Pen"
+  p.I_have_a "Apple"
+  p.uh! "Apple Pen"
+  p.I_have_a "Pen"
+  p.I_have_a "Pineapple"
+  p.uh! "Pineapple Pen"
+
+  p.apple_pen
+  p.pineapple_pen
+  p.uh! "Pen Pineapple Apple Pen"
+}
+
+PPAP.new.instance_exec {
+  I_have_a "Pen"
+  I_have_a "Apple"
+  uh! "Apple Pen"
+  I_have_a "Pen"
+  I_have_a "Pineapple"
+  uh! "Pineapple Pen"
+
+  apple_pen
+  pineapple_pen
+  uh! "Pen Pineapple Apple Pen"
+}
